65239134e199b987637b0604ef0f687513381fef
miguel.arosemena <miguel.arosemena.mt@usa.edu.co>
2022-09-22 19:57:19 -0500

    Se incluye un div hecho en canva y se comenta el js, trabaje en el proyecto-reto2.js

--- Proyecto-reto2.js
+++ Proyecto-reto2.js
@@ -1,20 +1,24 @@
+// Función de leer clientes a través del GET, acomodarlos en una lista
 function leerClientes(){
-    //FUNCION GET
+    //Información del GET
         $.ajax({    
-            url : 'https://g753758d201b824-gzl0jbaamzedshe9.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/costume/costume/id',
+            url : 'https://gdcbf67e56bd91d-motorent.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
             type : 'GET',
             dataType : 'JSON',
             contentType: 'application/json',
     
-            success : function(clientes) {
-                  /* let cs=clientes.items;
-                   $("#listaClientes").empty();
-                   for(i=0;i<cs.length;i++){
+            success : function(listaclientes) {
+                //traer los items de cliente y nombrarlos cs
+                let cs=listaclientes.items;
+                //Vacíar la lista antes de entrar al ciclo for de listar los clentes
+                $("#listaClientes").empty();
+
+                /* Ciclo for para buscar en el arreglo JSON
+                se usa el apend para unir*/
+                for(i=0;i<cs.length;i++){
                        $("#listaClientes").append(cs[i].id+" <b>"+cs[i].name+"</b> "+cs[i].email+" "+cs[i].age);
                        $("#listaClientes").append("<button onclick='borrarCliente("+cs[i].id+")'>Borrar</button><br>");
-                   */
-                   console.log(clientes.items)
-                   
+                }     
             },
             error : function(xhr, status) {
                 alert('ha sucedido un problema');
@@ -22,31 +26,35 @@ function leerClientes(){
         });
     }
     
-    
+// Función guardar/registar cliente  
     function guardarCliente() {
+        //Guarda los valores ingresados en los inputs del index
         let idCliente=$("#idCliente").val();
         let nombre=$("#nombreCliente").val();
         let mailCliente=$("#mailCliente").val();
         let edad=$("#edadCliente").val();
-    
+
+        // los guarda en un arreglo y los homologa con los nombres de las tablas
         let data={
             id:idCliente,
             name:nombre,
             email:mailCliente,
             age:edad
         };
-    
+        
+        //Envía los datos en formato JSON
         var dataToSend=JSON.stringify(data);
         console.log(data);
     
-    
+        //Crear cliente a través del handler POST
         $.ajax({    
             url : 'https://gdcbf67e56bd91d-motorent.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
             type : 'POST',
             dataType : 'JSON',
             data:dataToSend,
             contentType:'application/JSON',
-            success : function(pepito) {
+            success : function(ejecuciejecucion) {
+                //Una vez el cliente sea creado éxitosamente vacíar los inputs del html
                 $("#idCliente").val("");
                 $("#nombreCliente").val("");
                 $("#mailCliente").val("");
@@ -54,17 +62,17 @@ function leerClientes(){
                 console.log("Satisfactorio")
             },
             error : function(xhr, status) {
-               alert('ha sucedido un problema');
+               //alert('ha sucedido un problema');
             },
+            //Cuando se finaliza lo envía nuevamente a leerClientes, ejecutando nuevamente el GET
             complete: function(){
                leerClientes();
             }
         });
     
-    
     }
     
-    
+    //Editar es la misma lógica de crear cliente pero utilizando PUT
     function editarCliente(){
         let idCliente=$("#idCliente").val();
         let nombre=$("#nombreCliente").val();
@@ -85,7 +93,7 @@ function leerClientes(){
          //   dataType : 'json',
             data:dataToSend,
             contentType:'application/json',
-            success : function(pepito) {
+            success : function(ejecucion) {
                 $("#idCliente").val("");
                 $("#nombreCliente").val("");
                 $("#mailCliente").val("");
@@ -101,10 +109,13 @@ function leerClientes(){
     
     }
     
+
     function borrarCliente(idCliente){
+
+        //sólo se requiere el ID
         let data={
             id:idCliente
-        };
+            };
         let dataToSend=JSON.stringify(data);
         //console.log(dataToSend);
         $.ajax({    
